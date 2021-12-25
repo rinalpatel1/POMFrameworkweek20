db0b91aeda4da2b9f26bacd2068a0b89304c68e6
rinalpatel1 <92215174+rinalpatel1@users.noreply.github.com>
2021-12-25 22:51:16 +0000

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,14 @@
+package com.demo.nopcommerce.Utility;
+
+public class Constants {
+    public static final String LOGIN_PAGE_TITLE = "nopCommerce demo store. Login";
+
+    public static final String HOME_PAGE_TITLE =  "nopCommerce demo store";
+    public static final String HOME_PAGE_LOGOUT = "Log out";
+
+    public static final String REG_SHEET_NAME = "Registration";
+    public final static String REGISTER_PAGE_TITLE = "nopCommerce demo store. Register";
+
+
+
+}
